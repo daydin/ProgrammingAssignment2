85832169120afa4f4f27112c4d07f0ba47e39045
daydin <daydin@users.noreply.github.com>
2018-04-20 10:34:40 -0400

    Update makeCacheMatrix.R

--- makeCacheMatrix.R
+++ makeCacheMatrix.R
@@ -1,3 +1,6 @@
+## define below functions to set and get a matrix, as well as set and get its inverse. 
+## Together with cacheSolve.R they cache the inverse of a matrix to save user time in computing it every time it is needed. 
+
 makeCacheMatrix <- function(x = matrix()){
   inv <- NULL
   set <- function(y) {
